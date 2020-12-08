132f1386dc06f60cd6385497e251b3f51bf0853f
mddalam1 <mddalam1@gmail.com>
2020-12-08 14:24:06 +0530

    Creating Model for Collection and object for Document.

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ const courseSchema = new mongoose.Schema({
     date : { type : Date, default : Date.now },
     isPublished : Boolean
 })
+
+const Course = mongoose.model('Course',courseSchema);
+const course = new Course({
+    name : "NodeJS",
+    author : "Mosh Hamedani",
+    tags : [ "NodeJS", "JS", "Backend" ],
+    isPublished : true
+})
