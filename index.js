22fb92feeabc8b275370c1ad69c3864e039b80a6
mddalam1 <mddalam1@gmail.com>
2020-12-08 14:11:29 +0530

    Schema for a Collection in Mongoose.

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log("Connected to MongoDB..."))
     .catch(err => console.error(`Error : ${err.message}`))
+
+const courseSchema = new mongoose.Schema({
+    name : String,
+    author : String,
+    tags : [ String ],
+    date : { type : Date, default : Date.now },
+    isPublished : Boolean
+})
