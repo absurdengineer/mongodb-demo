d8e34a5b010ccc8787e003eb3fe6d3dd95338ab8
mddalam1 <mddalam1@gmail.com>
2020-12-10 12:34:10 +0530

    Dealed with Referencing Documents.

---
+++ population.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log(`Connected to MongoDB...`))
+    .catch(({name, message}) => console.error(`${name} : ${message}`))
+
+const Author = mongoose.model('Author',new mongoose.Schema({
+    name : String,
+    bio : String,
+    website : String
+}))
+
+const Course = mongoose.model('Course', new mongoose.Schema({
+    name : String,
+    author : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Author'
+    }
+}))
+
+const createAuthor = async (authorData) => {
+    const author = new Author(authorData)
+    const result = await author.save()
+    console.log(result)
+}
+const createCourse = async (courseData) => {
+    const course = new Course(courseData)
+    const result = await course.save()
+    console.log(result)
+}
+const listCourses = async () => {
+    const courses = Course
+        .find()
+        .select('name')
+    console.log(courses);
+}
+
+//* createAuthor({name : 'Mosh Hamedani', bio : 'Mosh\'s Bio', website : 'codewithmosh.com'})
+createCourse({name : 'Node.JS', author : "5fd1c6a0f5f764f4f01c7a3b"})
